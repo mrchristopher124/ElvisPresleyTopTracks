46d7f3a26397a5030de5416785012bac67ff6b9f
Christopher White <mrchristopher124@gmail.com>
2016-05-19 22:19:37 -0400

    set up initial views

--- ElvisPresleyTopTracks/ElvisPresleyTopTracks/ViewController.m
+++ ElvisPresleyTopTracks/ElvisPresleyTopTracks/ViewController.m
@@ -8,11 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource>
+
+@property(nonatomic, weak) IBOutlet UIView *headerView;
+@property(nonatomic, weak) IBOutlet UICollectionView *topTracksCollectionView;
 
 @end
 
 @implementation ViewController
+
+#pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,6 +27,19 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section;
+{
+    return 0;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
 }
 
 @end
