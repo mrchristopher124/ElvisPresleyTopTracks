674e5407971b24c032cf0649a8882e764a2257ef
Christopher White <mrchristopher124@gmail.com>
2016-05-19 22:09:00 -0400

    initial commit

---
+++ ElvisPresleyTopTracks/ElvisPresleyTopTracks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElvisPresleyTopTracks
+//
+//  Created by Christopher E White on 5/19/16.
+//  Copyright © 2016 Christopher White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ElvisPresleyTopTracks/ElvisPresleyTopTracks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ElvisPresleyTopTracks
+//
+//  Created by Christopher E White on 5/19/16.
+//  Copyright © 2016 Christopher White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
